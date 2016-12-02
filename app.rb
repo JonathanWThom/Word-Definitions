7321d1da574f74bf4d30346ad5b099c96850f0ef
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-12-02 08:28:57 -0800

    Add route to add word to homepage

--- app.rb
+++ app.rb
@@ -8,3 +8,10 @@ get('/') do
   @words = Word.all()
   erb(:index)
 end
+
+post('/') do
+  @word = params.fetch('word')
+  Word.new(:word => @word).save()
+  @words = Word.all()
+  erb(:index)
+end
---
+++ spec/word_integration_spec.rb
@@ -0,0 +1,13 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('The add new word route', {:type => :feature}) do
+  it('will add a new word to the homepage') do
+    visit('/')
+    fill_in('word', :with => 'hello')
+    click_button('Add Word')
+    expect(page).to have_content('hello')
+  end
+end
